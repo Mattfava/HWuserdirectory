51ba10ef88a40eb297042b94734f95e0f013c0f8
Matt Fava <matt.b.fava@gmail.com>
2020-08-13 16:10:42 -0400

    Updates

---
+++ precache-manifest.cf4a3496db5500de19f69f85cee76fd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ff5528b22d1a231843089d0bb3d72f",
+    "url": "/HWuserdirectory/index.html"
+  },
+  {
+    "revision": "f9236c3dbcc6e0449797",
+    "url": "/HWuserdirectory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b2806c18889f797fba0",
+    "url": "/HWuserdirectory/static/js/2.36989efa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HWuserdirectory/static/js/2.36989efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9236c3dbcc6e0449797",
+    "url": "/HWuserdirectory/static/js/main.e319bcce.chunk.js"
+  },
+  {
+    "revision": "90d193c2ffca42866ce6",
+    "url": "/HWuserdirectory/static/js/runtime-main.6e21146a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HWuserdirectory/static/media/logo.5d5d9eef.svg"
+  }
+]);
